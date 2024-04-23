1c1ac9785f129a598dbb22ffc63bb0759a9d5bed
zhengzonggang <zhengzonggang@qudian.com>
2024-04-23 18:23:58 +0800

    feat: add scroll paging feature for wheel

--- src/wheelPicker.tsx
+++ src/wheelPicker.tsx
@@ -1,25 +1,66 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { FlatList, StyleSheet, View, Text } from 'react-native';
 
 interface WheelPickerProps {
     wheelWidth: number;
     itemHeight: number;
-    data: number[];
+    data: string[];
     visibleNum?: 1 | 2 | 3;
 }
 
 const WheelPicker: React.FC<WheelPickerProps> = (props) => {
     const _visibleNum = props.visibleNum ?? 2;
 
+    const listRef = useRef<FlatList<string> | null>(null);
     const visibleNum = _visibleNum <= 3 ? _visibleNum : 3;
-    const listHeight = visibleNum * 2 + 1;
+    const listHeight = (visibleNum * 2 + 1) * props.itemHeight;
+
+    let data = props.data.slice();
+    let i = visibleNum;
+    while (i--) {
+        data.unshift('');
+        data.push('');
+    }
+
+    let maxOffsetY = data.length * props.itemHeight - listHeight;
+    maxOffsetY = maxOffsetY > 0 ? maxOffsetY : 0;
+
+    const scrollEndDrag = (event: {
+        nativeEvent: {
+            contentOffset: { x: number; y: number };
+        };
+    }) => {
+        const y = event.nativeEvent.contentOffset.y;
+        if (y < 0 || y > maxOffsetY) {
+            console.log('out range');
+            return;
+        }
+
+        const idx = Math.round(y / props.itemHeight);
+        listRef.current?.scrollToIndex({
+            animated: true,
+            index: idx,
+        });
+
+        console.log(`ff ${event.nativeEvent.contentOffset.y}`);
+    };
 
     return (
         <View style={[styles.container, { width: props.wheelWidth }]}>
+            <View
+                style={[
+                    styles.selector,
+                    { width: props.wheelWidth, height: props.itemHeight },
+                ]}
+            />
             <FlatList
+                ref={listRef}
+                overScrollMode="always"
+                showsVerticalScrollIndicator={false}
                 style={[styles.list, { maxHeight: listHeight }]}
-                pagingEnabled={true}
-                data={props.data}
+                data={data}
+                scrollEventThrottle={1}
+                onScrollEndDrag={scrollEndDrag}
                 renderItem={({ item }) => {
                     return (
                         <View
@@ -36,16 +77,24 @@ const WheelPicker: React.FC<WheelPickerProps> = (props) => {
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: 'blue',
+        justifyContent: 'center',
+        backgroundColor: '#EEEEEE',
+    },
+
+    selector: {
+        borderWidth: 1,
+        position: 'absolute',
     },
 
     list: {
         // backgroundColor: 'gray',
+        backgroundColor: 'transparent',
     },
     row: {
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#EEEEEE',
+        // backgroundColor: '#EEEEEE',
+        // borderWidth: 1
     },
     rowTitle: {
         color: 'black',
